89ffe5050f837260e2019c0253ed353afebbf1b9
Web-Geek <Web-Geek>
2021-06-07 00:57:09 +0530

    Updated upto counter design

--- src/pages/Mainpage/Mainpage.jsx
+++ src/pages/Mainpage/Mainpage.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
-import HomepageCSS from '../Homepage/HomepageCSS.module.css'
+import MainpageCSS from './MainpageCSS.module.css'
 
 const Mainpage = () => {
     const username = localStorage.getItem('username')
+    const imgurl ="https://fakeface.rest/thumb/view/"
     return (
-        <div className={`container ${HomepageCSS.container}`}>
-            <div className={`card ${HomepageCSS.card}`}>
-                <div className={HomepageCSS.imgdiv}>
-                    
+        <div className={`container ${MainpageCSS.container}`}>
+            <div className={`card ${MainpageCSS.card}`}>
+                <span className={MainpageCSS.txtspan}>
+                    <h4 className={MainpageCSS.h1}>Who would more likely to be the theif ?</h4>
+                </span>
+                <div className="row">
+                    <div className="col-6">
+                        <div className={MainpageCSS.imgdiv}>
+                            <img src={imgurl + '1'} alt="logo" className={MainpageCSS.img} />
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className={MainpageCSS.imgdiv}>
+                            <img src={imgurl + '2'} alt="logo" className={MainpageCSS.img} />
+                        </div>
+                    </div>
                 </div>
-                <h1 className={HomepageCSS.h1}>Brain Wave</h1>
                 
+                <span className={MainpageCSS.span}>
+                    <p className={MainpageCSS.counter}>30</p>
+                </span>
+
+                <div className="row mt-4">
+                    <div className="col-6">
+                        <div className={MainpageCSS.imgdiv}>
+                            <img src={imgurl + '3'} alt="logo" className={MainpageCSS.img} />
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className={MainpageCSS.imgdiv}>
+                            <img src={imgurl + '4'} alt="logo" className={MainpageCSS.img} />
+                        </div>
+                    </div>
+                </div>
+                <p className={MainpageCSS.p}>Brain Wave</p>
             </div>
         </div>
     )
